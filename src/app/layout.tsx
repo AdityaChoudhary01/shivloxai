951776ddef7750a24ea734b29d5e17a8d0a0cd96
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-10-10 23:14:55 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="ftdUSQ0-7RzVhdMCxpKJOqlfjqOVyCy_ee4WIEwSjG0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
