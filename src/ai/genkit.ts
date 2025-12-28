76f9cf1b0110b031fce0c5e5d20498b0d821377c
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-12-28 11:56:21 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,5 +5,5 @@ import { googleAI } from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [googleAI()],
   // Gemini 2.5 Flash is supported by this plugin
-  model: 'googleai/gemini-2.0-flash', 
+  model: 'googleai/gemini-2.5-flash-lite', 
 });
